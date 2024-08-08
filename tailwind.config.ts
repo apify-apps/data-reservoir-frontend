ee688cb44bb3c9a77017d8ae7db2aad380f2b8e9
Doctor Neuron <doctorneuron1718@gmail.com>
2024-08-08 23:30:25 +0700

    Feat: Bar chart pakai echarts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,33 @@ const config: Config = {
         "bluish": '#373A45',
         "bluish-200": '#17191f',
         "blackish": '#282B34',
+        "category": {
+          "transjakarta": '#184CA1',
+          "hayday": '#5A9E15',
+          "farmfrenzy": '#BA6609',
+          "pizzafrenzy": '#911D04',
+          "thesims": '#258018',
+          "nasigoreng": '#DFD3AF',
+          "default": '#5B7AD7'
+        },
+        "category-mid": {
+          "transjakarta": '#184CA155',
+          "hayday": '#5A9E1555',
+          "farmfrenzy": '#BA660955',
+          "pizzafrenzy": '#911D0455',
+          "thesims": '#25801855',
+          "nasigoreng": '#DFD3AF55',
+          "default": '#5B7AD755'
+        },
+        "category-hover": {
+          "transjakarta": '#184CA122',
+          "hayday": '#5A9E1522',
+          "farmfrenzy": '#BA660922',
+          "pizzafrenzy": '#911D0422',
+          "thesims": '#25801822',
+          "nasigoreng": '#DFD3AF22',
+          "default": '#5B7AD722'
+        }
       }
     },
   },
